16b1f39faed9ce489fdbce876247cbcf88f1ff38
Roundmilk <100121664+Roundmilk@users.noreply.github.com>
2025-01-13 10:59:01 +0800

    Update login.py

--- login.py
+++ login.py
@@ -11,8 +11,10 @@ aes_key = privateCrypt.get_aes_key()
 
 def get_credentials_path():
     if cf.CREDENTIALS_PATH is not None:
+        print "1"
         return cf.CREDENTIALS_PATH
     else:
+        print "1"
         home_path = os.getcwd()
         config_parent_path = os.path.join(home_path, 'myConfig')
         config_path = os.path.join(config_parent_path, 'credentials')
